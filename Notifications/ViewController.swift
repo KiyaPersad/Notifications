2fc8d03af974d1aece7e0647c6a297106d53d831
Kiya Persd <s5062311@bournemouth.ac.uk>
2018-11-27 15:10:45 +0000

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Kiya Persad on 27/11/2018.
+//  Copyright © 2018 Kiya Persad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
